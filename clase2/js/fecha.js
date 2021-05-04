b8ae92fb7e94acbd5d3523ac2caf4adaa457e42f
Marcos <exegeses@gmail.com>
2021-05-03 21:55:54 -0300

    fecha utilizando switch

--- clase2/js/fecha.js
+++ clase2/js/fecha.js
@@ -1,10 +1,36 @@
-    //mostrar fecha formato: 03/05/2021
+    //mostrar fecha formato: Lunes 03/05/2021
 
     //ubicamos elemento dentro del DOM
     var txt = document.getElementById('txt');
 
     //creamos objeto de fecha
     var fecha = new Date();
+
+    //obtenemos día de la semama | del 0 al 6
+    var diaSemana = fecha.getDay();
+    switch( diaSemana ){
+        case 0:
+            diaSemana = 'Domingo';
+            break;
+        case 1:
+            diaSemana = 'Lunes';
+            break;
+        case 2:
+            diaSemana = 'Martes';
+            break;
+        case 3:
+            diaSemana = 'Miércoles';
+            break;
+        case 4:
+            diaSemana = 'Jueves';
+            break;
+        case 5:
+            diaSemana = 'Viernes';
+            break;
+        default:
+            diaSemana = 'Sábado';
+            break;
+    }
 
     //obtenemos dia del mes de 1 al 31
     var diaMes = fecha.getDate();
@@ -20,6 +46,27 @@
     var anio = fecha.getFullYear();
 
     // escribimos dentro del span
-    txt.innerText = diaMes+'/'+mes+'/'+anio;
+    txt.innerText = diaSemana+diaMes+' '+'/'+mes+'/'+anio;
 
 
+/*
+    if( diaSemana == 0 ){
+        diaSemana = 'Domingo';
+    }else if( diaSemana == 1 ){
+        diaSemana = 'Lunes';
+    }else if( diaSemana == 2 ){
+        diaSemana = 'Martes';
+    }else if( diaSemana == 3 ){
+        diaSemana = 'Miércoles'
+    }else if( diaSemana == 4 ){
+        diaSemana = 'jueves';
+    }
+*/
+
+/*
+    switch ( varibleTestigo ){
+        case x:
+            algo;
+            break;
+    }
+*/
